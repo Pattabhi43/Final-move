8cec907d8e8dcab4c630d0852dc4bbc91f05d87e
Pattabhi43 <kpramaiah46@gmail.com>
2023-06-06 19:54:22 +0530

    Added provider configuration

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.44.0"
+    }
+  }
+
+cloud {
+    organization = "AWS_prk"
+
+    workspaces {
+      name = "Conf-2"
+    }
+  }
+}
+
+
+variable "AWS_ACCESS_KEY" {
+  description = "aws access key"
+}
+
+variable "AWS_SECRET_KEY_ID" {
+  description = "aws secret key"
+}
+
+variable "API-token" {
+  description = "api token tf cloud"
+}
+
+provider "tfe" {
+  token = var.API-token
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY_ID
+}
+
+data "aws_ami" "ai-2" {
+  owners = ["137112412989"]
+  most_recent = true
+}
